84fd8ee784b118da4aa45635f0970d32417e33a9
ovinenterprisesmain-cell <ovinenterprises.main@gmail.com>
2026-03-12 14:37:29 +0530

    IPC example.

---
+++ main.js
@@ -0,0 +1,40 @@
+// const { app, BrowserWindow } = require('electron')
+
+// const createWindow = () => {
+//   const win = new BrowserWindow({
+//     width: 720,
+//     height: 1280
+//   })
+
+//   win.loadFile('index.html')
+// }
+
+// app.whenReady().then(() => {
+//   createWindow()
+// })
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+
+
+
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+})
